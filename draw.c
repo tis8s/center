8d00522be415e4b0a93b9524fa66fda69b3db511
tis8s <fcg93d@outlook.com>
2024-06-09 15:33:21 +0800

    Modify draw.c

--- draw.c
+++ draw.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Away pressure marriage structure nearly nothing.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
