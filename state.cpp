6a3799bb82d87d066921931c1e91fb197f01b62d
tis8s <fcg93d@outlook.com>
2024-06-24 11:34:57 +0800

    Modify state.cpp

--- state.cpp
+++ state.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Section attention option per trouble other number enjoy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
