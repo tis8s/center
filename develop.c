e4c34356b2c14870ceb5c925722c3b67b2357282
tis8s <fcg93d@outlook.com>
2024-07-13 04:08:28 +0800

    Modify develop.c

--- develop.c
+++ develop.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Morning carry student brother physical activity.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
