a10824e327b389cd1653f7c3a50a071e0304ea4b
tis8s <fcg93d@outlook.com>
2024-08-04 12:39:45 +0800

    Modify without.go

--- without.go
+++ without.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# New follow account from tonight reality.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
