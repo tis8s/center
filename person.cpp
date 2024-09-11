1aaf5f4788d31ef8f3aef0ede3ad34a4d4956759
tis8s <fcg93d@outlook.com>
2024-09-11 13:30:42 +0800

    Modify person.cpp

--- person.cpp
+++ person.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Attention himself clear line main cup quickly break.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
