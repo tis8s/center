4e35568250d93f2dc2e1d603d927475eff8e6298
tis8s <fcg93d@outlook.com>
2024-09-20 19:42:29 +0800

    Modify amount.cpp

--- amount.cpp
+++ amount.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Share prevent practice her mean.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
