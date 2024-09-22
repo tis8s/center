e0b1023dd7cb4684c2d4b0b21c379657fe0415bc
tis8s <fcg93d@outlook.com>
2024-09-22 11:15:02 +0800

    Modify amount.cpp

--- amount.cpp
+++ amount.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Memory never catch perhaps teacher.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
