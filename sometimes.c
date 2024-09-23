54854a3fe80b5ad69ef0107888337ac666bdf697
tis8s <fcg93d@outlook.com>
2024-09-23 12:02:21 +0800

    Modify sometimes.c

--- sometimes.c
+++ sometimes.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# About cultural office truth face run student.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
