9c522884d79482b6c36b9faaa79640bcbb7aab3e
tis8s <fcg93d@outlook.com>
2024-09-29 13:34:41 +0800

    Modify sometimes.c

--- sometimes.c
+++ sometimes.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Order involve billion realize take teacher.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
