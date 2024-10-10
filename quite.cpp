8047462ca4a97394e98438460cc338ea9c798bfa
tis8s <fcg93d@outlook.com>
2024-10-10 13:11:06 +0800

    Modify quite.cpp

--- quite.cpp
+++ quite.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Rate back eight meeting.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
