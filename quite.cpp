ac0c905fe51038ff26a385649a8ca2ee0a0572d3
tis8s <fcg93d@outlook.com>
2024-10-12 10:06:11 +0800

    Modify quite.cpp

--- quite.cpp
+++ quite.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Human people century risk.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
