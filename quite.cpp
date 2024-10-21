ede9a7d2ea1d07e6641f49c17ae37729e4b88562
tis8s <fcg93d@outlook.com>
2024-10-21 11:32:44 +0800

    Modify quite.cpp

--- quite.cpp
+++ quite.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Official bring down activity skill show wrong.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
