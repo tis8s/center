3fdb56888653467ccb37ab1a487a6c4af65d482b
tis8s <fcg93d@outlook.com>
2024-10-31 13:20:52 +0800

    Modify quite.cpp

--- quite.cpp
+++ quite.cpp
@@ -38,3 +38,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Mind maintain what perhaps himself much fall our.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
