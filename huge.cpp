62ebe126967e2f37fb4d8c7c4f2b2121e272e71f
tis8s <fcg93d@outlook.com>
2024-11-08 11:42:04 +0800

    Modify huge.cpp

--- huge.cpp
+++ huge.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Around especially share think.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
