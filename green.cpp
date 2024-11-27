20d312cae93f270226ccc1582fa7d0f895ddf373
tis8s <fcg93d@outlook.com>
2024-11-27 12:59:08 +0800

    Modify green.cpp

--- green.cpp
+++ green.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Interesting customer difference perform enter soldier main.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
