f185ca51a24d108451b40e9c94f7f053ffd45a83
tis8s <fcg93d@outlook.com>
2024-12-06 15:09:22 +0800

    Modify before.cpp

--- before.cpp
+++ before.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Several real prepare final at thought west.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
