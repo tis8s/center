2545bafdc4a6e2f5e2b3f11e60efdedf931b5c6f
tis8s <fcg93d@outlook.com>
2025-01-15 12:06:50 +0800

    Modify there.cpp

--- there.cpp
+++ there.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Few situation simply against.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
