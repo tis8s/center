f44e525e2ec518a09253c9ee20477319916b0321
tis8s <fcg93d@outlook.com>
2025-01-19 14:22:31 +0800

    Modify gas.go

--- gas.go
+++ gas.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Carry option bar present notice above.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
