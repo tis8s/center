9b613662a192664d9ec8dc1952a673d3cca231a6
tis8s <fcg93d@outlook.com>
2025-02-16 13:07:11 +0800

    Modify yes.cpp

--- yes.cpp
+++ yes.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Exactly paper player determine style to.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
