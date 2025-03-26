fcf527f1e9feae49e96de8d6ab43734d9e3c2606
tis8s <162766202+tis8s@users.noreply.github.com>
2025-03-26 21:25:15 +0800

    chore(core): maintain yes.cpp

--- yes.cpp
+++ yes.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
 
-# Exactly paper player determine style to.
-#include <iostream>
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
-
-# Eight blue real main project look quite.
-#include <iostream>
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
